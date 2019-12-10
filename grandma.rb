7456bfe1ede9e106098e82442f030dc2f1af1db6
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-10 03:32:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -20,10 +20,13 @@ def speak_to_grandma(phrase ="I LOVE YOU GRANDMA!")
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   
+  elsif phrase == phrase.downcase
+    return "HUH?! SPEAK UP, SONNY!"
+  
   elsif phrase == phrase.uppercase
     return "NO, NOT SINCE 1938!"
   
- else phrase == phrase.downcase
+ else
     return "HUH?! SPEAK UP, SONNY!"
 
 end
